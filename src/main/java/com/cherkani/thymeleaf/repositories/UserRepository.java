2cbe6d4c0daa80e35e7478a15df38bc8b3b35f95
Aymen Cherkani <cherkaniaymen1@gmail.com>
2023-12-04 22:04:30 +0100

    comm

---
+++ src/main/java/com/cherkani/thymeleaf/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cherkani.thymeleaf.repositories;
+
+import com.cherkani.thymeleaf.beans.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
